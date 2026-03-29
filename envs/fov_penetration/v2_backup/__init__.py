61c0ac6879658894ac96a7c888e6909e0c58e188
fpocheese <940346202@qq.com>
2026-03-29 13:01:41 +0800

    chore: prune historical versions and keep latest v21

--- envs/fov_penetration/v2_backup/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-FOV Penetration Environment Package
-"""
-
-from .fov_penetration_env import FOVPenetrationEnv
-from .config import get_config, DEFAULT_CONFIG
-
-__all__ = ["FOVPenetrationEnv", "get_config", "DEFAULT_CONFIG"]
